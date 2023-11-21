ec80c66840c2aab1a012d862c42e98663e197295
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-11-21 10:48:36 +0530

    add initial project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Akshay Kumar on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
